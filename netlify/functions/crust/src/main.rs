2a570a27da068ab9db66e7e2c7597a5267504c13
Vladimir <josephjdpollard@gmail.com>
2026-02-05 02:39:13 +0400

    fix: correct lambda_http handler signature using service_fn and run

--- netlify/functions/crust/src/main.rs
+++ netlify/functions/crust/src/main.rs
@@ -1,23 +1,23 @@
-use lambda_http::{NextEvent, Request, Response};
-use lambda_runtime::{service_fn, Error, LambdaEvent};
+use lambda_http::{run, Body, Error, Request, Response};
+use lambda_runtime::service_fn;
 use log::LevelFilter;
 use simple_logger::SimpleLogger;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     SimpleLogger::new().with_utc_timestamps().with_level(LevelFilter::Info).init().unwrap();
-
-    let func = service_fn(my_handler);
-    lambda_runtime::run(func).await?;
+    run(service_fn(my_handler)).await?;
     Ok(())
 }
 
-pub(crate) async fn my_handler(event: LambdaEvent<Request<String>>) -> Result<Response<String>, Error> {
-    let path = event.payload.uri().path();
+async fn my_handler(event: Request) -> Result<Response<Body>, Error> {
+    let path = event.uri().path();
 
     let resp = Response::builder()
         .status(200)
-        .body(format!("Alloha from '{}' good", path))?;
+        .header("content-type", "text/plain")
+        .body(Body::Text(format!("Alloha from '{}' good", path)))
+        .map_err(|e| Error::from(e.to_string()))?;
 
     Ok(resp)
 }
